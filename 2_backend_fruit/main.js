edb38a32f7d7c610cbc1a2b88c5e12941a70473a
Meszaros Nandor <meszaros.nandor@BOLYAI.LOCAL>
2025-05-23 15:32:36 +0200

    orai cucc

---
+++ 2_backend_fruit/main.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('fruitform')
+const fruit = document.getElementById("fruitname")
+const price =  document.getElementById('fruitprice')
+
+form.addEventListener('submit', (e) =>
+{
+    e.preventDefault()
+    const obj = {
+        name: fruit.value,
+        price: price.value
+    }
+
+    fetch("http://127.0.0.1:63013/fruits", {method:"post", headers:{"Content-Type": "application/json"}, body:JSON.stringify(obj)}).then((response)=>{
+        const resopnse = response.json()
+        console.log(resopnse)
+    })
+})
+
+
